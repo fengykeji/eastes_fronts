ce4a802e1309528b1cbf9a6ec65a1f2dbe898ee4
Zoidbergme <suxun1202@gmail.com>
2018-05-24 02:00:10 +0800

    no vuex

--- src/components/AllInOneManagement/AddNav.vue
+++ src/components/AllInOneManagement/AddNav.vue
@@ -6,33 +6,39 @@
             <el-button class="btn" @click="cancel">取消</el-button>
         </div>
         <div class="content">
-            <el-form ref="form" :model="form" label-width="80px">
+            <el-form ref="form" :model="form" label-width="100px" class="left">
                 <el-form-item label="二级栏目名称">
                     <el-input v-model="form.name"></el-input>
                 </el-form-item>
                 <el-form-item label="设置序号">
                     <el-input v-model="form.sort"></el-input>
                 </el-form-item>
-                <el-upload class="avatar-uploader" action="https://jsonplaceholder.typicode.com/posts/" :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
-                    <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                <el-upload class="avatar-uploader" :action="url + '/user/project/upload'" :show-file-list="false" name="img_url" :http-request="customUpload">
+                    <img v-if="showUrl" :src="showUrl" class="avatar">
                     <i v-else class="el-icon-plus avatar-uploader-icon"></i>
                 </el-upload>
-
             </el-form>
         </div>
-        <form id="formid">
+        <!-- <form id="formid">
             <label for="resource">上传文档</label>
             <input type="file" id="resource" name="resource" ref="resource">
             <button type="button" @click="upload()">上传</button>
-        </form>
+        </form> -->
     </div>
 </template>
 
 <script>
+import url from '../../../static/url.js'
+import showImgServer from "../../../static/showImgServer.js";
+import qs from 'qs'
+
 export default {
   name: "addNav",
   data: function() {
     return {
+      url:url.url,
+      imageUrl:'',
+      showUrl:'',
       form: {
         name: "",
         sort: ""
@@ -40,55 +46,115 @@ export default {
     };
   },
   methods: {
-    submit() {},
-    cancel() {},
-    upload() {
-      var formData = new FormData(); // FormData 对象
-      formData.append("img_url", this.$refs.resource.files[0]);
-    //   formData.append("name", "foo");
-    //   formData.append("sort", 5);
-    //   formData.append("pid", 0);
-    //   formData.append("project_id", 1);
-
-      this.$http.post('http://192.168.0.154/hs_agent/public/index.php/user/project/upload',formData,{
-        //   method:'post',
-          headers:{
-              'Content-Type': 'multipart/form-data'
-          }
-      }).then((res)=>{
+    submit() {
+        if(!(this.imageUrl && this.form.name && this.form.sort)){
+            this.$message('请填写相应数据')
+            return 
+        }
+        let data = {
+            name:this.form.name,
+            sort:this.form.sort,
+            pid:0,
+            img_url:this.imageUrl,
+            project_id:1
+        }
+        this.$http({
+            url:this.url+'/user/project/addNav',
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded"
+            },
+            data: qs.stringify(data)
+        }).then((res)=>{
+            this.$router.push({name:'AIOSetting'})
+        },()=>{
+            this.$message('网络错误')
+        })
+    },
+    cancel() {
+        this.$router.go(-1)
+    },
+    customUpload(file){
+        let fd = new FormData();
+        fd.append('img_url',file.file)
+        this.$http({
+        method: "post",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        url:this.url + '/user/project/upload',
+        // url:
+        //   "http://192.168.0.154/hs_agent/public/index.php/user/project/uploadLogo",
+        data: fd
+      }).then(res => {
+          this.imageUrl = res.data.data.img_url
         //   console.log(res)
-        return res.data.data.img_url
-      }).then((res)=>{
-
-         return this.$http({
-              url:'http://192.168.0.154/hs_agent/public/index.php/user/project/addNav',
-              method:'POST',
-              headers:{
-                //   'Content-Type': 'multipart/form-data',
-                  'Content-Type': 'application/x-www-form-urlencoded',
-                //   'Content-Type': 'application/json',
-              },
-              data:{
-                   name:'foo',
-                  sort:5,
-                  pid:0,
-                  img_url:res,
-                  project_id:1
-              }
-          })
-      }).then((result)=>{
-          console.log(result)
-      })
+          this.showUrl = showImgServer.showImgServer + res.data.data.img_url;
+      });
     }
+    // upload() {
+    //   var formData = new FormData(); // FormData 对象
+    //   formData.append("img_url", this.$refs.resource.files[0]);
+    //   //   formData.append("name", "foo");
+    //   //   formData.append("sort", 5);
+    //   //   formData.append("pid", 0);
+    //   //   formData.append("project_id", 1);
+
+    //   this.$http
+    //     .post(
+    //       "http://192.168.0.154/hs_agent/public/index.php/user/project/upload",
+    //       formData,
+    //       {
+    //         //   method:'post',
+    //         headers: {
+    //           "Content-Type": "multipart/form-data"
+    //         }
+    //       }
+    //     )
+    //     .then(res => {
+    //       //   console.log(res)
+    //       return res.data.data.img_url;
+    //     })
+    //     .then(res => {
+    //       let qs = require("qs");
+    //       let foo = {
+    //         name: "foo",
+    //         sort: 5,
+    //         pid: 0,
+    //         img_url: res,
+    //         project_id: 1
+    //       };
+
+    //       return this.$http({
+    //         url:
+    //           "http://192.168.0.154/hs_agent/public/index.php/user/project/addNav",
+    //         method: "POST",
+    //         headers: {
+    //           "Content-Type": "application/x-www-form-urlencoded"
+    //         },
+    //         data: qs.stringify(foo),
+    //         // transformRequest: [
+    //         //   function(data) {
+    //         //     return qs.stringify(data);
+    //         //   }
+    //         // ]
+    //       });
+    //     })
+    //     .then(result => {
+    //       console.log(result);
+    //     });
+    // }
   }
 };
 </script>
 
 <style scoped>
+p{
+    margin: 0;
+}
 #madd {
   max-width: 600px;
   margin: 0 auto;
   margin-top: 200px;
+  border: 1px solid #dcdcdc
 }
 .mtitle {
   background-color: #333333;
@@ -99,13 +165,14 @@ export default {
   height: 50px;
   line-height: 50px;
   font-size: 14px;
+  margin-bottom: 20px;
 }
 .txt {
   flex-grow: 1;
   color: aliceblue;
 }
 .avatar-uploader .el-upload {
-  border: 1px dashed #d9d9d9;
+  border: 5px dashed #d9d9d9;
   border-radius: 6px;
   cursor: pointer;
   position: relative;
@@ -117,14 +184,19 @@ export default {
 .avatar-uploader-icon {
   font-size: 28px;
   color: #8c939d;
-  width: 178px;
-  height: 178px;
-  line-height: 178px;
+  width: 120px;
+  height: 120px;
+  line-height: 120px;
   text-align: center;
 }
 .avatar {
-  width: 178px;
-  height: 178px;
+  width: 120px !important;
+  height: 120px !important;
   display: block;
 }
+div.el-upload.el-upload--text{
+    width: 120px;
+    height: 120px;
+}
+
 </style>
---
+++ static/url.js
@@ -0,0 +1,3 @@
+export default {
+	url:"http://192.168.0.154/hs_agent/public/index.php"
+}
